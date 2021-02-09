e1e11248ea16917856ffb048da249fa69dba1bfb
NuuJoy <63086412+NuuJoy@users.noreply.github.com>
2021-02-09 23:23:13 +0700

    Add files via upload

--- vPadDS4_client.py
+++ vPadDS4_client.py
@@ -3,10 +3,17 @@
 import time
 import json
 from nuuJoyLib.Pythonista.touchpad import touchGamePad
+from nuuJoyLib.Pythonista.sensor import motion_sensor
 from nuuJoyLib.Socket.tcpipv4 import client_socket
+from nuuJoyLib.HWCtrl.acquisition import constantRateAcquisition
 
 
 __version__ = (2021,2,9,'beta')
+
+
+#server_address = ('172.20.10.9',13666)
+server_address = ('192.168.254.118',13666)
+enable_imu_sensor = True
 
 
 class socketjsonput():
@@ -65,18 +72,28 @@ if __name__ == '__main__':
 
     ds4pad.run()
 
-    for retry in range(5):
-        if not(ds4pad.stop_event.is_set()):
-            print('connecting to server ... (retry: {})'.format(retry))
-            try:
-                with client_socket('172.20.10.9',13666) as client:
-                    scktjson.client = client
-                    while client.conn_status() and not(ds4pad.stop_event.is_set()):
-                        time.sleep(1.0)
-                    scktjson.client = socketjsonput.fake_client()
-            except:
-                pass
-            time.sleep(1.0)
+
+    while not(ds4pad.stop_event.is_set()):
+        print('connecting to server ...')
+        try:
+            with client_socket(*server_address) as client:
+                scktjson.client = client
+                while client.conn_status() and not(ds4pad.stop_event.is_set()):
+                    if enable_imu_sensor:
+                        with motion_sensor() as sensor:
+                            def emit_attitude():
+                                scktjson.send_event('attitude',{'name':'imu_sensor','roll':sensor.attitude['roll'],
+                                                                                    'pitch':sensor.attitude['pitch'],
+                                                                                    'yaw':sensor.attitude['yaw']})
+                            dataAcqs = constantRateAcquisition(func_list=(emit_attitude,),output_list=(None,),
+                                                            rate_limit=50,stop_event=ds4pad.stop_event)
+                            dataAcqs.start_acquiring()
+                    time.sleep(1.0)
+                scktjson.client = socketjsonput.fake_client()
+        except Exception as err:
+            print('Exception: ',err)
+            scktjson.client = socketjsonput.fake_client()
+        time.sleep(1.0)
             
     print('terminate.')
 
